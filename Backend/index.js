4ecb80ac14d2b9d76c234b3b7d7e4507144298ec
Ankit Modi <ankitmodi2428@gmail.com>
2026-02-06 23:51:46 +0530

    .env error is solve

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import dotevn from 'dotenv';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authrouter from './Route/route.js';
 import promtrouter from './Route/promt_route.js'
 import cookieParser  from 'cookie-parser';
 import cors from 'cors'
 
-dotevn.config();
+dotenv.config();
 const app = express();
 const port  = process.env.PORT || 4001;
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI ;
 
 mongoose.connect(uri).then(()=>console.log("Connected to Mongodb")).catch((error)=>console.log("mongo dp connection error: ",error));
 
@@ -28,7 +28,7 @@ app.use(cors({
 app.use("/api/v1/user",authrouter)
 app.use("/api/v1/user",promtrouter)
 
-app.get('/test',(req,res)=>{
+app.get('/test',(_req,res)=>{
     res.json({message: 'API is working'});
 });
 
